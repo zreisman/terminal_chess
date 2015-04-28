7c2147a0ebd964f154e3b5bde49f19d7017f9f03
Zachary M. Reisman <zach.reisman@gmail.com>
2015-04-28 15:50:03 -0700

    Let sliding pieces see available moves

--- w2d2/chess/pieces/piece.rb
+++ w2d2/chess/pieces/piece.rb
@@ -16,17 +16,60 @@ class Piece
     @moved = false
   end
 
+  def inspect
+    {
+      display: @display,
+      pos: @pos,
+      moved: @moved,
+      color: @color
+    }
+  end
+
   def moves
      raise "Method not yet defined in pieces.rb"
   end
 
-  def valid_move?
-     raise "Method not yet defined in pieces.rb"
+  def valid_move?(pos)
+     on_board?(pos) &&
+      (@board[pos].nil? || @board[pos].color != @color)
+  end
+
+  def on_board?(pos)
+    x, y = pos
+    x.between?(0,7) && y.between?(0,7)
   end
 end
 
 class SlidingPiece < Piece
-  attr_reader :move_powers
+
+  def moves
+    available_moves = []
+
+    MOVE_DIRS.each do |dir|
+      (1..7).each do |slide_amount|
+        move_vector = multiply_vector(dir, slide_amount)
+        test_pos = add_vector(self.pos , move_vector )
+
+        if valid_move?( test_pos  )
+          available_moves << test_pos
+          break if board[test_pos] # captured other piece
+        else
+          break
+        end
+      end
+    end
+    available_moves
+
+  end
+
+  def add_vector(pos, dir)
+    pos.zip(dir).map{ |x,y| x + y }
+  end
+
+  def multiply_vector(dir, slide_amount)
+    dir.map{|x, y| [x * slide_amount, y * slide_amount] }
+  end
+
 
 
 end
